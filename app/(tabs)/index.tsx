68d81e1a3e7b13360b4b69fa19f4acf2b5166756
anukul rai <anukul.rau@gmail.com>
2025-07-23 19:55:59 +0545

    changes done for Tabbar

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { StatusBar, Text, View } from "react-native";
+import {styles} from '../../styles/auth.styles'
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <>
+    <StatusBar barStyle={'light-content'}/>
+    <View style={styles.container}>
+      <Link href='/notification'><Text>Visit Notification</Text></Link>
+    </View>
+    </>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{flex:1}}>
+      <Stack screenOptions={{
+        headerShown:false
+      }}>
+        <Stack.Screen name="index" options={{
+          title:'Home',
+          // headerShown:false,
+        }}/>
+        <Stack.Screen name="notification" options={{
+          title:'Notification',
+          // headerShown:false,
+        }}/>
+      </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
+);
 }
